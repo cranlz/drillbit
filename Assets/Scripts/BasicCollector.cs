6f0b9b0970fcdbd358582d1a0d811479ee519331
Connor Lantz <cal0064@auburn.edu>
2020-09-22 14:24:46 -0500

    Added TMP and URP packages, improved collector

--- Assets/Scripts/BasicCollector.cs
+++ Assets/Scripts/BasicCollector.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BasicCollector : MonoBehaviour
 {
     public float rateOfCollection = 1.0f;
     public int collectionAmount = 1;
     public int bank = 0;
+    public GameObject text;
+    private TextMeshPro textMesh;
     void Start()
     {
+        textMesh = text.GetComponent<TextMeshPro>();
         InvokeRepeating("Collect", rateOfCollection, rateOfCollection);
     }
 
     private void Collect()
     {
         bank += collectionAmount;
+        textMesh.SetText("{0}", bank);
     }
 }
