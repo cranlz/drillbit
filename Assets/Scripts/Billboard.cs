f2b0c090fc1ffa2fc8653930b19e34ddd6750d55
Connor Lantz <cal0064@auburn.edu>
2020-09-24 22:10:45 -0500

    Player sprite!

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class Billboard : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+         transform.forward = Camera.main.transform.forward;
+    }
+}
