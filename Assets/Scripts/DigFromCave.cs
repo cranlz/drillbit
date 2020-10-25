cc86a53368520fe96e70d23d54b7b908348ec0ee
Connor Lantz <cal0064@auburn.edu>
2020-10-25 16:52:40 -0500

    DIGGING WORKS

--- Assets/Scripts/DigFromCave.cs
+++ Assets/Scripts/DigFromCave.cs
@@ -3,32 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using Sebastian.Geometry;
 using ClipperLib;
+using System.Runtime;
 
-public class DigFromCave : MonoBehaviour
-{
+public class DigFromCave : MonoBehaviour {
     public ShapeCreator caveMeshes;
+    public Shape cookieCutter = new Shape();
+    public float circAng = 10f;
+    public float circRad = 5f;
+    public bool snip = false;
 
-    private void OnMouseUp()
+    private void Start() {
+
+    }
+
+    private void Update()
     {
-        //var cookieCutter = List<IntPoint>
-        
-        Mesh digObject = gameObject.GetComponent<MeshFilter>().mesh;
-        Debug.Log(digObject.name);
+        //Update our cookiecutter points
+        cookieCutter.points.Clear();
+        var ang = 0f;
+        while (ang < 360f) {
+            var posX = circRad * Mathf.Sin(ang * Mathf.Deg2Rad) + gameObject.transform.position.x;
+            var posY = circRad * Mathf.Cos(ang * Mathf.Deg2Rad) + gameObject.transform.position.z;
+            cookieCutter.points.Add(new Vector3(posX, 0f, posY));
+            ang += circAng;
+        }
 
-        var shapeToCut = ShapeToPath(caveMeshes.shapes[0]);
-        var cookieCutter = new List<IntPoint>();
-        cookieCutter.Add(new IntPoint(gameObject.transform.position.x+2, gameObject.transform.position.z));
-        cookieCutter.Add(new IntPoint(gameObject.transform.position.x, gameObject.transform.position.z));
-        cookieCutter.Add(new IntPoint(gameObject.transform.position.x, gameObject.transform.position.z+2));
-        cookieCutter.Add(new IntPoint(gameObject.transform.position.x+2, gameObject.transform.position.z+2));
+        if (snip) {
+            snip = false;
+            var shapeToCut = ShapeToPath(caveMeshes.shapes[1]);
+            var cookieCutterShape = ShapeToPath(cookieCutter);
 
-        List<List<IntPoint>> solution = new List<List<IntPoint>>();
-        Clipper c = new Clipper();
-        c.AddPath(shapeToCut, PolyType.ptSubject, true);
-        c.AddPath(cookieCutter, PolyType.ptClip, true);
-        c.Execute(ClipType.ctDifference, solution, PolyFillType.pftEvenOdd, PolyFillType.pftEvenOdd);
-        caveMeshes.shapes[0] = PathToShape(solution[0]);
-        caveMeshes.UpdateMeshDisplay();
+            var solution = new List<List<IntPoint>>();
+            var c = new Clipper();
+
+            if (!c.AddPath(shapeToCut, PolyType.ptSubject, true)) Debug.Log("path 1 failure");
+            if (!c.AddPath(cookieCutterShape, PolyType.ptClip, true)) Debug.Log("path 2 failure");
+
+            if (!c.Execute(ClipType.ctUnion, solution, PolyFillType.pftEvenOdd, PolyFillType.pftEvenOdd)) Debug.Log("solution failure");
+
+            caveMeshes.shapes[1] = PathToShape(solution[0]);
+            caveMeshes.UpdateMeshDisplay();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        foreach( var i in cookieCutter.points)
+        {
+            Gizmos.DrawSphere(i, 0.1f);
+        }
     }
 
     //Convert our polygon to a list of IntPoints
@@ -37,8 +60,15 @@ public class DigFromCave : MonoBehaviour
         var points = new List<IntPoint>();
         for (var i = 0; i < shape.points.Count; i++)
         {
-            points.Add(new IntPoint(shape.points[i].x, shape.points[i].z));
+            //Convert each float to a long, but retain 4 decimal points of accuracy
+            var s = string.Format("{0:0.0000}", shape.points[i].x);
+            long posX = long.Parse(s.Replace(".", ""));
+            s = string.Format("{0:0.0000}", shape.points[i].z);
+            long posY = long.Parse(s.Replace(".", ""));
+            //Debug.Log(posX + " " + posY);
+            points.Add(new IntPoint(posX, posY));
         }
+        Debug.Log(points.Count);
         return points;
     }
 
@@ -48,14 +78,12 @@ public class DigFromCave : MonoBehaviour
         var shape = new Shape();
         for (var i = 0; i < points.Count; i++)
         {
-            shape.points.Add(new Vector3(points[i].X, 0f, points[i].Y));
+            //Convert each long back into a float, and get back our 4 decimal points
+            var posX = points[i].X / 10000f;
+            var posY = points[i].Y / 10000f;
+            //Debug.Log(posX + " " + posY);
+            shape.points.Add(new Vector3(posX, 0f, posY));
         }
         return shape;
     }
-
-    /*
-    public Paths ClipperSolution(ClipType type, ) {
-
-    }
-    */
 }
